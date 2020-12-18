cb8d62060988e834592a260bf9e5929ead1fe380
DiegoVsp <diego-vespa2020@outlook.com.br>
2020-12-17 21:48:10 -0300

    primeiro commit

---
+++ Ex_5/Ex_5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace Ex_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int codPeca, numPeca;
+            float valUniP, total = 0;
+            int i = 0;
+
+            while (i < 2)
+            {
+                Console.WriteLine("Digite o código da peça");
+                codPeca = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite o quantidade da peça");
+                numPeca = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite o valor unitário da peça");
+                valUniP = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                total += valUniP * numPeca;
+                i++;
+            }
+
+
+            Console.WriteLine("O total é de R${0}", total.ToString("F2"), CultureInfo.InvariantCulture);
+        }
+    }
+}
