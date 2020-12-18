b2760e971eb3746b78583d854a3de1684d633e3b
DiegoVsp <diego-vespa2020@outlook.com.br>
2020-12-18 12:39:55 -0300

    att exercicios

---
+++ Ex_6/Ex_6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace Ex_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, areaTri, areaCirc, areaQ, areaR, areaTra, pi = 3.14159;            
+
+
+            Console.WriteLine("Digite um valor para A");
+            a = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite um valor para B");
+            b = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite um valor para c");
+            c = float.Parse(Console.ReadLine());
+
+            areaTri = a * c / 2;
+            areaCirc = pi * (c * c);
+            areaQ = b * b;
+            areaR = a * b;
+            areaTra = (a + b) / 2.0 * c;
+
+            Console.WriteLine("Área Triângulo: {0}",areaTri.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área Circulo: {0}", areaCirc.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área Quadrado: {0}", areaQ.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área Retângulo: {0}", areaR.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área Trapézio: {0}", areaTra.ToString("F3", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
