56e1ca6745b5f8cd047831a803e7e3b896ff91f6
DiegoVsp <diego-vespa2020@outlook.com.br>
2020-12-21 21:20:46 -0300

    atualizando conteudo

---
+++ EstruturaCond/EstruturaCond/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EstruturaCond
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hora;
+            Console.WriteLine("Qual a hora atual? ");
+            hora = int.Parse(Console.ReadLine());
+
+            if(hora < 12)
+            {
+                Console.WriteLine("Bom dia!");
+            }else if(hora < 18)
+            {
+                Console.WriteLine("Boa tarde!");
+            }else
+            {
+                Console.WriteLine("Boa Noite!");
+            }
+
+        }
+    }
+}
